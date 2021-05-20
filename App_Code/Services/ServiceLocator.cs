c70f8aa8687daf4c177f5cb665151efb4dfca523
luljam <lucianofdebrito@gmail.com>
2021-05-20 19:12:27 -0300

    First commit

---
+++ App_Code/Services/ServiceLocator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Collections.Generic;
+
+/// <summary>
+/// Summary description for ServiceLocator
+/// </summary>
+public class ServiceLocator : IServiceLocator
+{
+    public Dictionary<object, object> services = null;
+
+    public ServiceLocator()
+    {
+        services = new Dictionary<object, object>();
+        this.services.Add(typeof(ICidRepository), new CidRepository());
+    }
+
+    public T GetService<T>()
+    {
+        try
+        {
+            return (T)services[typeof(T)];
+        }
+        catch (KeyNotFoundException)
+        {
+            throw new ApplicationException("O serviço solicitado não esta registrado.");
+        }
+    }
+}
---
+++ Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+public partial class _Default : System.Web.UI.Page
+{
+
+    IServiceLocator locator = new ServiceLocator();
+    protected ICidRepository repo;
+
+    public _Default()
+    {
+        repo = locator.GetService<ICidRepository>();
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        GridView1.DataSource = repo.Listar();
+        GridView1.DataBind();
+    }
+}
+
